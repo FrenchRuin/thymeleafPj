7b762166674a762d6acb505df477800ff851a10c
이재찬 <toxic023@naver.com>
2022-10-05 23:40:09 +0900

    first success

--- src/main/java/com/example/soloproject/controller/WebController.java
+++ src/main/java/com/example/soloproject/controller/WebController.java
@@ -9,19 +9,37 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @Slf4j
 public class WebController {
 
-    @RequestMapping(value = "test",method = RequestMethod.GET)
+    @RequestMapping(value = "test", method = RequestMethod.GET)
     public String test(Model model) {
         model.addAttribute("webForm", new WebDto());
         log.info("okokoko");
         return "index";
     }
 
+    @RequestMapping(value = "test2", method = RequestMethod.GET)
+    public String test2(Model model, WebDto webDto) {
+        webDto.setAddress("ddddd");
+        webDto.setName("ddddd");
+        return "index";
+    }
 
+    @RequestMapping(value = "test3", method = RequestMethod.POST)
+    public String test3(HttpServletRequest request, Model model, WebDto webDto) {
+        model.addAttribute("webDto", new WebDto());
+        log.info(webDto.getName());
+        log.info(webDto.getAddress());
+        return "redirect:/test2";
+    }
 
-
-
+    @RequestMapping(value = "test4", method = RequestMethod.GET)
+    public String test4(Model model, WebDto webDto) {
+        WebDto webDto1 = new WebDto();
+        return "index";
+    }
 }
