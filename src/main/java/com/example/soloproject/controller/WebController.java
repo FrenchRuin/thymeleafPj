cdd6761303d4f0f44ddb2734ec41c309c519c5eb
이재찬 <toxic023@naver.com>
2022-10-06 22:22:04 +0900

    repo

--- src/main/java/com/example/soloproject/controller/WebController.java
+++ src/main/java/com/example/soloproject/controller/WebController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 @Controller
 @Slf4j
