dded8c5f581e02e670fdf17b2fce7836938addaf
이재찬 <toxic023@naver.com>
2022-10-11 21:25:50 +0900

    page decoration

--- src/main/java/com/example/soloproject/utils/ModelMapperUtils.java
+++ src/main/java/com/example/soloproject/utils/ModelMapperUtils.java
@@ -4,6 +4,8 @@ import org.modelmapper.ModelMapper;
 
 public class ModelMapperUtils {
 
+    /* model mapper utils */
+
     private static final ModelMapper mapper = new ModelMapper();
 
     public static ModelMapper getMapper() {
