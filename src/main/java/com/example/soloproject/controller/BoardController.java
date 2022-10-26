9adc7fc418ffed2160e9757ea2afdaf608d5eec4
FrenchRuin <toxic023@naver.com>
2022-10-26 18:34:09 +0900

    board

--- src/main/java/com/example/soloproject/controller/BoardController.java
+++ src/main/java/com/example/soloproject/controller/BoardController.java
@@ -1,16 +1,22 @@
 package com.example.soloproject.controller;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@Slf4j
 public class BoardController {
 
-    @RequestMapping(value = "/board",method = RequestMethod.GET)
+    @RequestMapping(value = "/board", method = RequestMethod.GET)
     public String board() {
         return "board";
     }
 
+    @RequestMapping(value = "/board/add", method = RequestMethod.POST)
+    public void addBoard() {
+        log.info("addBoard");
+    }
 }
