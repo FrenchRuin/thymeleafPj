b8a27db6ba90c51f50ba911884e4bfd4f33330d0
이재찬 <toxic023@naver.com>
2022-11-02 19:31:16 +0900

    security add user

--- src/main/java/com/example/soloproject/config/WebSecurityConfig.java
+++ src/main/java/com/example/soloproject/config/WebSecurityConfig.java
@@ -4,19 +4,53 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig  {
+public class WebSecurityConfig  { // WebSecurityConfigurerAdapter is deprecated.
 
 
+    // Configuring HttpSecurity
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        return http.build();
+//    }
 
-    // WebSecurityConfigurerAdapter is deprecated.
+    //Configuring WebSecurity
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//        return web -> web.ignoring().antMatchers();
+//    }
+
+    // Password Encode
     @Bean
-    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.formLogin().disable();
-        return http.build();
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        final PasswordEncoder pw = passwordEncoder();
+        UserDetails user1 = User.builder()
+                .username("user1")
+                .password(pw.encode("1234"))
+                .roles("USER")
+                .build();
+        UserDetails user2 = User.builder()
+                .username("user2")
+                .password(pw.encode("1234"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
     }
 
 }
